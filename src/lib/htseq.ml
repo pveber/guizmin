0164cfe106ded23d0068fe91495776ec871755d0
pveber <philippe.veber@gmail.com>
2014-12-11 07:44:19 +0100

    htseq: added pysam installation and dep to count

--- src/lib/htseq.ml
+++ src/lib/htseq.ml
@@ -4,6 +4,12 @@ open Workflow.API
 
 let ( % ) = Fn.compose
 
+let pysam_package_script = Utils.wget "https://raw.githubusercontent.com/pveber/compbio-scripts/master/pysam-install/0.8.1/pysam-install.sh"
+
+let pysam_package = workflow [
+    bash pysam_package_script [ target () ]
+  ]
+
 let package_script = Utils.wget "https://raw.githubusercontent.com/pveber/compbio-scripts/master/htseq-install/0.6.1p1/htseq-install.sh"
 
 let package = workflow [
@@ -35,7 +41,7 @@ let count ?order ?mode ?stranded ?feature_type ?minaqual ?idattribute alns gff =
     | `bam bam -> "bam", dep bam
   in
   workflow [
-    program ~pythonpath:[package] ~path:[package] ~stdout:(target ()) "htseq-count" [
+    program ~pythonpath:[package ; pysam_package] ~path:[package] ~stdout:(target ()) "htseq-count" [
       opt "-f" string format ;
       option (opt "-m" (string % string_of_mode)) mode ;
       option (opt "-r" (string % string_of_order)) order ;
